ddc8897a3722a59c2fd27bd87aca557e636b107d
rkr-dev <object.prototype.js@gmail.com>
2020-09-08 02:31:37 -0400

    Navbar Component and Hero Section
    Navbar Component and the Hero
    Section Setup

--- src/pages/index.js
+++ src/pages/index.js
@@ -1,11 +1,35 @@
 import React from "react"
-import { Link } from "gatsby"
-import SEO from "../components/seo"
+import withStyles from "@material-ui/core/styles/withStyles";
+import Header from '../assets/material-kit/components/Header/Header';
+import HeaderLinks from "../assets/material-kit/components/Header/HeaderLinks";
 
-const IndexPage = () => (
-  <div>
-    <h1>Home Page</h1>
-  </div>
-)
+import landingPageStyle from "../assets/material-kit/views/landingPage";
+import HeroSection from "../components/HeroSection";
 
-export default IndexPage
+const dashboardRoutes = [];
+
+class LandingPage extends React.Component {
+  render() {
+    const {classes, ...rest} = this.props;
+
+    return (
+      <div>
+        <Header 
+        color="transparent"
+        routes={dashboardRoutes}
+        brand="Recall Graph"
+        rightLinks={<HeaderLinks />}
+        fixed
+        changeColorOnScroll={{
+          height:400,
+          color:'white'
+        }}
+        {...rest}
+        />
+        <HeroSection />
+      </div>
+    )
+  }
+}
+
+export default withStyles(landingPageStyle)(LandingPage)
