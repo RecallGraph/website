b3238c2820f0cbff6d78cf8b09346f66431714c9
rkr-dev <object.prototype.js@gmail.com>
2020-09-29 10:59:06 -0400

    Added Media Queries
    Added Media Queries

--- src/assets/material-kit/views/productStyle.jsx
+++ src/assets/material-kit/views/productStyle.jsx
@@ -4,6 +4,40 @@ const productStyle = {
   section: {
     padding: "30px 0",
     textAlign: "center",
+    '@media only screen and (min-width: 1920px)': {
+      h2: {
+        fontSize: "2.2rem !important" 
+      },
+      p:{
+        fontSize:"2.2rem !important",
+        textAlign:`center !important`,
+        fontFamily:"sans-serif",
+        lineHeight:"1.3"
+      },
+      h4:{
+        fontSize:"1.4rem"
+      },
+      description:{
+        textAlign:"center !important"
+      }
+    },
+    '@media only screen and (-webkit-min-device-pixel-ratio: 2)': {
+      h2: {
+        fontSize: "2.2rem !important" 
+      },
+      p:{
+        fontSize:"2.2rem !important",
+        textAlign:`center !important`,
+        fontFamily:"sans-serif",
+        lineHeight:"1.3"
+      },
+      h4:{
+        fontSize:"1.4rem"
+      },
+      description:{
+        textAlign:"center !important"
+      }
+    },
   },
   title: {
     ...title,
@@ -14,6 +48,7 @@ const productStyle = {
   },
   description: {
     color: "#999",
+    textAlign:"left"
   },
 }
 
